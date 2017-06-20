02da0ec79a2fd572a4a8d8b59c1fdb5336a7c0e5
Amir Eldor <amir@eize.ninja>
2017-06-21 02:41:43 +0300

    Drop 'days back' query and put randomized noun+adj query

--- fetchPigeons.py
+++ fetchPigeons.py
@@ -1,15 +1,20 @@
 import settings
 import urllib.request
-from random import randint
+from random import choice
 
 
 def makeRequestUrl(settings):
+    nouns = ("pigeon", "dove", "pigeonhole")
+    adjectives = ("", "nice", "pretty", "lovely", "elegant", "real", "flying", "eating")
+    pigeon_query = "{}%20{}".format(choice(adjectives), choice(nouns))
+
     return "https://www.googleapis.com/customsearch/v1" \
-           "?q=pigeon&cx={}&imgType=photo&dateRestrict=d{}" \
+           "?q={}&cx={}&imgType=photo" \
            "&safe=medium&searchType=image&fields=items%2Flink&key={}" \
-           .format(settings.CSE_ID, randint(0, 100), settings.API_KEY)
+           .format(pigeon_query, settings.CSE_ID, settings.API_KEY)
 
 request_url = makeRequestUrl(settings)
+print(request_url)
 
 response = urllib.request.urlopen(request_url)
 raw = response.read()
