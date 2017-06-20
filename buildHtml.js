e3b1e73ca7651785642d2269d4ffc91130df75e1
Amir Eldor <amir@eize.ninja>
2017-06-21 02:06:28 +0300

    node script for creating html from pigeons json

---
+++ buildHtml.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fromGoogle = require('./pigeons.json')
+const pigeons = fromGoogle.items.map(item => {
+    return item.link;
+})
+
+html = makeHtml(pigeons)
+
+fs.writeFile("index.html", html, (err, file) => {
+    if (err) {
+        throw err
+    }
+    console.log("File written")
+})
+
+function makeHtml(pigeons) {
+    let html = "<!doctype html><title>Pigeon Pictures</title><meta charset=\"utf-8\">"
+    html += `<style>body { background: url(${pigeons[0]}); background-size: cover; text-align: center }</style>`
+    for (let link of pigeons) {
+        html += `<img alt="pigeon" src="${link}">`
+    }
+    return html
+}
